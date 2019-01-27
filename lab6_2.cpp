d238c235f1ad08b4ca9cff006d9a23a6128e0d81
neokarn <34811101+neokarn@users.noreply.github.com>
2019-01-27 20:07:28 +0700

    Create lab6_2.cpp

---
+++ lab6_2.cpp
@@ -0,0 +1,18 @@
+//Dialog of the first example is given below.
+
+/*
+Fahsai: Sawadee ka...Can you tell me your name?
+?????: Luke Skywalker
+Fahsai: Wow!!! Luke Skywalker that is a really cool name.
+Fahsai: I think you are Engineering student. What is your student ID?
+Luke Skywalker: 590610999
+Fahsai: I think you may be GEAR 47.  I have a free movie tickets for you. 
+Fahsai: Let's go to cinema together!!! 
+Fahsai: What movie do you want to watch?
+Luke Skywalker: Star Wars VII
+Fahsai: So....which day are you free to go with me?
+Luke Skywalker: Next Monday
+Fahsai: Next Monday....that is OK!!! I'm looking forward to watch Star Wars VII with you.
+Luke Skywalker: May the Force be with you krub
+Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/ 
+*/
