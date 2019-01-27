49ade93dfd41e7ed19c5e807a45e3b844c97b450
neokarn <34811101+neokarn@users.noreply.github.com>
2019-01-27 20:08:30 +0700

    Create lab6_3.cpp

---
+++ lab6_3.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+char before(char x){
+	//Write your function definition here
+}
+
+int main(){
+	//Test Case
+	cout << before('A') << "\n";
+	cout << before('B') << "\n";
+	cout << before('P') << "\n";
+	cout << before('T') << "\n";
+	cout << before('Z') << "\n";
+	cout << before('a') << "\n";
+	cout << before('0') << "\n";
+	cout << before('c') << "\n";
+	return 0;
+}
