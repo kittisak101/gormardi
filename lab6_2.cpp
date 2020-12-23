811564a56c7ea3dcecf203933b374abbfcb6aea9
neokarn <34811101+neokarn@users.noreply.github.com>
2020-12-23 20:12:50 +0700

    Update lab6_2.cpp

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -3,16 +3,16 @@
 /*
 Fahsai: Sawadee ka...Can you tell me your name?
 ?????: Luke Skywalker
-Fahsai: Wow!!! Luke Skywalker that is a really cool name.
-Fahsai: I think you are Engineering student. What is your student ID?
+Fahsai: Wow!!! Luke Skywalker is a really cool name.
+Fahsai: I think you are an Engineering student. What is your student ID?
 Luke Skywalker: 590610999
-Fahsai: I think you may be GEAR 47.  I have a free movie tickets for you. 
-Fahsai: Let's go to cinema together!!! 
+Fahsai: I think you may be GEAR 47. I have a free movie ticket for you.
+Fahsai: Let's go to the cinema together!!!
 Fahsai: What movie do you want to watch?
 Luke Skywalker: Star Wars VII
 Fahsai: So....which day are you free to go with me?
 Luke Skywalker: Next Monday
-Fahsai: Next Monday....that is OK!!! I'm looking forward to watch Star Wars VII with you.
+Fahsai: Next Monday....that is OK!!! I'm looking forward to watching Star Wars VII with you.
 Luke Skywalker: May the Force be with you krub
-Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/ 
+Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/
 */
