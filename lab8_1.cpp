0b22f36fb9af4b7387d93c278805d3c94d956f47
kittisak101 <kittiisak_b@cmu.ac.th>
2022-12-20 15:23:13 +0700

    finish

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-
+using namespace std;
 int main() {
-    int a  =  20; b = 30;
-    int c = 4, output;
+    int a  =  20, b = 30;
+    float c = 4, output;
 
-    outpot = a - b/c;
-    count << output: << output << \n;
+    output = a - b/c;
+    cout << "output: " << output << "\n";
     return 0;
 }
